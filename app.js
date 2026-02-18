e4c6e3adc27ed6ae2d12a86d62961fab1965dd22
Liad Gross <liadgr@umich.edu>
2026-02-18 09:55:15 +0100

    Milestone4

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      message: ''
-    }
-  },
-  computed: {
-    htmlOutput() {
-      return marked.parse(this.message)
+    data() {
+        return {
+            message: '',
+            theme: 'light'
+        }
     },
-    charCount() {
-      return this.htmlOutput.replace(/<[^>]*>/g, '').length
+    computed: {
+        htmlOutput() {
+            return marked.parse(this.message)
+        },
+        charCount() {
+            return this.htmlOutput.replace(/<[^>]*>/g, '').length
+        },
+        themeIcon() {
+            return this.theme === 'light' ? 'dark_mode' : 'light_mode'
+        }
+    },
+    mounted() {
+        const saved = localStorage.getItem('markdownText')
+        if (saved) {
+            this.message = saved
+        }
+
+        const savedTheme = localStorage.getItem('theme')
+        if (savedTheme) {
+            this.theme = savedTheme
+            document.documentElement.setAttribute('data-bs-theme', savedTheme)
+        }
+    },
+    watch: {
+        message(newValue) {
+            localStorage.setItem('markdownText', newValue)
+        },
+        theme(newValue) {
+            localStorage.setItem('theme', newValue)
+        }
+    },
+    methods: {
+        toggleTheme() {
+            this.theme = this.theme === 'light' ? 'dark' : 'light'
+            document.documentElement.setAttribute('data-bs-theme', this.theme)
+        }
     }
-  }
 })
 
 app.mount('#app')
